a09d22fe514219e55080e5a07978f9a6747fc91b
hengfeng.zhuo <hengfeng.zhuo@rokid.com>
2019-03-24 14:14:14 +0800

    Initial commit

---
+++ app/src/main/java/com/ryan/musicplayer/MainActivity.java
@@ -0,0 +1,21 @@
+package com.ryan.musicplayer;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.TextView;
+
+import com.ryan.musicplayer.music.core.Demo;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        // Example of a call to a native method
+        TextView tv = findViewById(R.id.sample_text);
+        tv.setText(new Demo().stringFromJNI());
+    }
+
+}
